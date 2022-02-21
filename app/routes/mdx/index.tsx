9e53a3182474b9b7847df6d3ba40070de9194600
Simon Krause <simon+github@skrause.me>
2022-02-21 22:07:59 +0100

    feat: add rudimentary mdx capabilities

---
+++ app/routes/mdx/index.tsx
@@ -0,0 +1,11 @@
+import Component from "~/../content/test.mdx"
+
+export default function Index() {
+  return (
+    <div>
+      <h1>MDX TEST</h1>
+      The Component:
+      <Component />
+    </div>
+  )
+}
